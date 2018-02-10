631d8e6ec2c7f840c65f00f859c7309d2044467f
CenTea <34390309+CenTea@users.noreply.github.com>
2018-02-10 14:05:41 -0800

    Youtube streaming, Partially implemented Queue
    Can play music and Can somewhat queue, Has double dispatcher end issue.

--- BasilBot.js
+++ BasilBot.js
@@ -14,8 +14,11 @@ const token = process.env.BASIL_TOKEN;
 const curses = ['fucking','fuck','fucked','fck','fkc','shit','bitch'];
 const prefix = 'Basil';
 var pick = [];
-var dispatcher = null;
+dispatcher = null;
+playlist = [];
+mob = [];
 
+//
 // Planned closeness levels.
 // >-100 - 200, 201 - 600, 601 - 1000+ 
 
@@ -40,7 +43,7 @@ function filter(message, cursearray)
 {
 	var stringer = message.content.toLowerCase();
 	var pooresp = ['Poo states: I will come for you at night!', 'Poo calmly stares and smiles at ' + at(message) , 'Poo grins at ' + at(message) , 'Poo winks at ' + at(message), 'Poo flexes'];
-
+	var cursechange = ['Oh Flowerbeds!', 'By the great pixie dust!', 'Ghost Signals!', 'Bloody Cheek!', 'Wanking Peeker!', 'If it not be Bone Storm!'];
 	for (i = 0; i <	cursearray.length ; i++)
 	{
 		if (stringer.includes(cursearray[i]))
@@ -48,8 +51,62 @@ function filter(message, cursearray)
 			message.channel.send('No Swearing! '+ at(message) + '\nThe message has been deleted.');
 			message.channel.send(imageresp(message,'http://i0.kym-cdn.com/photos/images/original/001/237/094/21c.jpg', 'Swearing is bad!', pooresp[Math.floor(Math.random()*pooresp.length)]) );
 			message.delete();
+			message.reply('Here\'s a recommended replacement: ' + cursechange[Math.floor(Math.random()*cursechange.length)]);
 			break;
 		}
+	}
+}
+
+function play(message, url)
+{
+		const channel = message.member.voiceChannel;
+		if (channel)
+		{
+			channel.join();
+			const streamOptions = { seek: 0, volume: 1 };
+		try{
+			const stream = ytdl(url, { filter : 'audioonly' });
+			dispatcher = channel.connection.playStream(stream, streamOptions);
+			message.reply('Basil is Playing!~');
+		dispatcher.on('error', console.error);
+		dispatcher.on("end", end => {
+			message.reply("Basil has completed your request!~");
+			playqueue();
+		}); // end dispatcher
+		}catch(err){
+			console.log(err);
+			message.channel.send("URL Error!");
+			if (playlist.length>0)
+			{
+			dequeue();
+			}
+		} // end try-catch
+
+		} // end if
+		else 
+		{
+			message.reply('Channel?');
+		} // end else
+}
+
+function enqueue(url, message)
+{
+	playlist.push(url);
+	mob.push(message);
+}
+
+function dequeue()
+{
+	playlist.shift();
+	mob.shift();
+}
+
+function playqueue()
+{
+	if (playlist.length>0)
+	{
+	play(mob[0],playlist[0]);
+	dequeue();
 	}
 }
 
@@ -86,6 +143,17 @@ if (message.author.username != 'Basil Hem')
 			message.reply('No.');
 		}
 		break;
+	case 'end': case 'stop': case 'next':
+		if (dispatcher == null)
+		{
+			message.channel.send('Nothing is playing?');
+		}
+		else
+		{
+			dispatcher.pause();
+			dispatcher.end();
+		}
+		break;
 	case 'exit':
 		try{
 		message.member.voiceChannel.leave();
@@ -114,32 +182,33 @@ if (message.author.username != 'Basil Hem')
 			message.channel.send(pick[Math.floor(Math.random()*pick.length)] + 'was chosen.');
 		}
 		break;
+	case 'queue':case 'q':
+		if (command[1] != undefined)
+		{
+			enqueue(command[1], message);
+			message.reply('Queued');
+		}
+		else
+		{
+			message.reply('Error... Maybe?');
+		}
+		break;
 	case 'play':
-		const channel = message.member.voiceChannel;
-		if (channel)
+		if (command[1] != undefined)
 		{
-			channel.join();
-			const streamOptions = { seek: 0, volume: 1 };
-		try{
-			const stream = ytdl(command[1], { filter : 'audioonly' });
-			dispatcher = channel.connection.playStream(stream, streamOptions);
-			message.channel.send('Playing~');
-		dispatcher.on('error', console.error);
-        	dispatcher.on("end", end => {
-          		message.channel.send("Request Complete~");
-			dispatcher = null;
-		}); // end dispatcher
-
-		}catch(err){
-			console.log(err);
-			message.channel.send("URL Error!");
-		} // end try-catch
-
-		} // end if
-		else 
+		play(message, command[1]);
+		}
+		else
 		{
-			message.reply('Channel?');
-		} // end else
+			if (playlist.length>0)
+			{
+			playqueue();
+			}
+			else
+			{
+			message.channel.send('The playlist seems to be empty.');
+			}
+		}
 		break;
 	case 'removeme': case 'remove me':
 		const index = pick.indexOf(at(message));
